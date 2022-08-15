57ac74ee39a5bbdbc18a2db4066b0c398243952e
aayushi <aayushishah2972@gmail.com>
2022-08-15 13:18:38 -0400

    Last finishing touches

--- routes/posts.js
+++ routes/posts.js
@@ -6,74 +6,74 @@ const connectEnsureLogin = require('connect-ensure-login');
 
 
 //getting all the posts
-router.get("/",(req,res)=>{
-    Post.find({},(err,allPosts)=>{
-        if(err){
+router.get("/", (req, res) => {
+    Post.find({}, (err, allPosts) => {
+        if (err) {
             console.log(err)
-        }else{
-            res.render("posts/index.ejs",{
-                posts:allPosts
+        } else {
+            res.render("posts/index.ejs", {
+                posts: allPosts
             })
         }
     })
 })
 
 //display form to make a new post
-router.get("/new",connectEnsureLogin.ensureLoggedIn(),(req,res)=>{
+router.get("/new", connectEnsureLogin.ensureLoggedIn(), (req, res) => {
     res.render("posts/new.ejs")
 })
 
 //create a new post
-router.post("/",connectEnsureLogin.ensureLoggedIn(),(req,res)=>{
-    var title=req.body.title
-    var body=req.body.body
+router.post("/", connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+    var title = req.body.title
+    var body = req.body.body
 
-    var newPost={
-        title:title,
-        body:body
+    var newPost = {
+        title: title,
+        body: body
     }
-    Post.create(newPost,(err,newlyCreated)=>{
-        if(err){
+    Post.create(newPost, (err, newlyCreated) => {
+        if (err) {
             console.log(err)
-        }else{
+        } else {
             console.log(newlyCreated)
-            req.flash("success","Successfully Updated !!!")
-            res.redirect("/posts")
+            req.flash("success", "Successfully Updated !!!")
+            res.redirect("/")
         }
     })
 })
 
 //editing
-router.get('/:id/edit',connectEnsureLogin.ensureLoggedIn(),(req, res) => {
-    Post.findById(req.params.id,(err,foundPost)=>{
-        res.render("posts/edit",{post:foundPost})
+router.get('/:id/edit', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+    Post.findById(req.params.id, (err, foundPost) => {
+        res.render("posts/edit", { post: foundPost })
     })
 });
 
-router.put("/:id",connectEnsureLogin.ensureLoggedIn(),(req, res) => {
+router.put("/:id", connectEnsureLogin.ensureLoggedIn(), (req, res) => {
     console.log(req.body.title)
-    var newPost={
-        title:req.body.title,
-        body:req.body.body
+    var newPost = {
+        title: req.body.title,
+        body: req.body.body
     }
-    Post.findByIdAndUpdate(req.params.id,newPost,(err,post)=>{
-        if(err){
+    Post.findByIdAndUpdate(req.params.id, newPost, (err, post) => {
+        if (err) {
             console.log(err)
-        }else{
+        } else {
             // console.log(post)
-            res.redirect("/posts")
+            res.redirect("/")
         }
     })
 })
 
 //deleteing a post
-router.delete("/:id",connectEnsureLogin.ensureLoggedIn(),(req, res) => {
-    Post.findByIdAndDelete(req.params.id,(err,delPost)=>{
-        if(err){
+router.delete("/:id", connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+    Post.findByIdAndDelete(req.params.id, (err, delPost) => {
+        if (err) {
             console.log(err)
-        }else{
+        } else {
             console.log(delPost)
-            res.redirect("/posts")
+            res.redirect("/")
         }
     })
 })
